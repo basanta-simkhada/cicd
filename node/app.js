93b31ed301de40bd15e1b4e3c8fb577ef31785ac
Basanta <basanta.simkhada@alphabox.com.np>
2026-06-30 18:15:16 +0545

    node and java code modified 1

--- node/app.js
+++ node/app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("This is a node app");
+  res.end("This is a node app.");
 });
 
 server.listen(PORT, HOST, () => {
