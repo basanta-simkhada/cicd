775d63b15de1ed6e1e6189c9a8c17846369626c6
Basanta <basanta.simkhada@alphabox.com.np>
2026-07-08 18:41:22 +0545

    testing

--- node/app.js
+++ node/app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("This is a node app. v2");
+  res.end("This is a node app. v1");
 });
 
 server.listen(PORT, HOST, () => {
